3400da773da53ff0fcce10f9e1bd9e76ff4eda4c
aatthhuurr <2411500025@mahasiswa.atmaluhur.ac.id>
2026-06-04 15:23:19 +0700

    Layout & Java: Menyediakan wadah kosong LoginActivity untuk tim back-end

---
+++ app/src/main/java/com/aatthhuurr/mobisuas_2411500025/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.aatthhuurr.mobisuas_2411500025;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+// Import komponen biar dak error
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText edtNoHp, edtPassword;
+    private MaterialButton btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        // Kode default bawaan Android Studio Quail untuk padding layar
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutMainLogin), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // ======================================================================
+        // TUGAS BACK-END TIM:
+        //lanjutkan casting findViewById dan logika onClick API di bawah nih ok ges!
+        // ======================================================================
+
+    }
+}
